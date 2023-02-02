531bc847eed739af032d9b317a440bdf30dcd9de
kanwar Dhillon <kdhillon469@gmail.com>
2023-02-02 12:22:38 +1100

    Revert "bug fixes"
    This reverts commit 89c01714709acaab4346e0dedb7de397012febc1.

---
+++ SoccerPlayerApi/Data/Repositories/PlayerRepository.cs
@@ -0,0 +1,53 @@
+﻿using SoccerPlayerApi.Data.Intefaces;
+
+namespace SoccerPlayerApi.Data.Repositories
+{
+
+    public class PlayerRepository : IPlayerRepository
+    {
+        private readonly SoccerPlayersContext _Context;
+
+        public PlayerRepository(SoccerPlayersContext dbContext)
+        {
+
+            _Context = dbContext;
+        }
+
+        public async Task<int> CreatePlayer(Player player)
+        {
+            _Context.Players.Add(player);
+         return   await _Context.SaveChangesAsync();
+           // return player.PlayerId;
+        }
+
+        public async Task<bool> DeletePlayer(int playerId)
+        {
+            var player = await _Context.Players.Where(x => x.PlayerId == playerId).FirstOrDefaultAsync();
+            _Context.Players.Remove(player);
+            await _Context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<int> EditPlayer(Player player)
+        {
+            var playerToEdit = await _Context.Players.Where(x => x.PlayerId == player.PlayerId).FirstOrDefaultAsync();
+            playerToEdit.PlayerName = player.PlayerName;
+            playerToEdit.JeresyNumber = player.JeresyNumber;
+            await _Context.SaveChangesAsync();
+            return player.PlayerId;
+        }
+
+        public async Task<List<Player>> GetPlayers()
+        {
+            var result = await _Context.Players.ToListAsync();
+            return result;
+        }
+
+        //public async Task<bool> Save()
+        //{
+        //    var result = await _Context.SaveChangesAsync();
+        //   return  result > 0 ? true : false;
+        //}
+    }
+
+}
